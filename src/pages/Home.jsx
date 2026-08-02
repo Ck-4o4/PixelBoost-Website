17071c7600be4fd8e4b89f870bb1e78c38a4dd10
Ck-4o4 <ckxads@gmail.com>
2026-08-02 19:27:40 +0530

    remove viral reels & shoots from home page, keep exclusively in Work page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,6 @@ import Hero from '../components/Hero';
 import Clients from '../components/Clients';
 import Expertise from '../components/Expertise';
 import MetaAdsGallery from '../components/MetaAdsGallery';
-import VideoGallery from '../components/VideoGallery';
 import Process from '../components/Process';
 
 const Home = () => {
@@ -15,11 +14,6 @@ const Home = () => {
       <section id="meta-ads" style={{ background: '#FAFAFA', padding: '5rem 0' }}>
         <div className="container">
           <MetaAdsGallery />
-        </div>
-      </section>
-      <section id="viral-reels" style={{ background: '#FFFFFF', padding: '5rem 0' }}>
-        <div className="container">
-          <VideoGallery />
         </div>
       </section>
       <Process />
